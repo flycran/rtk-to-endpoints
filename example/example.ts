b3680f4fbaf7cd9c9f809e40d3e6fc2f74f71ae3
flycran <cffhidol@163.com>
2026-03-20 15:07:45 +0800

    - 修复案例

--- example/example.ts
+++ example/example.ts
@@ -1,6 +1,6 @@
-import { useGetUserByIdQuery, userApi } from "./userApi";
+import { useGetProductsQuery, userApi } from "./userApi";
 import { userApi2, useGetUserByIdQuery as useGetUserById2Query } from "./userApi2";
-useGetUserByIdQuery
+useGetProductsQuery
 useGetUserById2Query
 userApi.useGetUsersQuery
 userApi2.useGetUserByIdQuery
